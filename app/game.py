b4d297d69c47a1c9be5ac1d2b9b792ea88963ee7
Gregory Vaumourin <gregory.vaumourin@gmail.com>
2022-11-22 09:53:42 +0100

    Add a config file for parameters

--- app/game.py
+++ app/game.py
@@ -1,20 +1,37 @@
-import time
-import serial
 from threading import Thread
 
+import sys
+import time
+import json
+import serial
 
-ser = serial.Serial(
-#    port='/dev/ttyACM0',\
-    port='COM7',\
-    baudrate=115200,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-    timeout=0)
 
-print("connected to: " + ser.portstr)
- 
+##### Default Config
+config = {
+    "device" : "COM7",
+    "question_file" : "questions.txt",
+    "nb_players" : 4,
+    "points_per_good_answer" : 1,
+    "points_per_wrong_answer" : 0
+}
 
+
+def serial_init():
+    ser = serial.Serial(
+    #    port='/dev/ttyACM0',\
+        port=config["device"],\
+        baudrate=115200,\
+        parity=serial.PARITY_NONE,\
+        stopbits=serial.STOPBITS_ONE,\
+        bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+    if ser.isOpen():
+        print("connected to: " + ser.portstr)
+    else:
+        print("ERROR: Could not connect to the Arduino board on the device:" + config["device"])
+        sys.exit()
+    return ser
 
 def switch_led(button_index , is_turn_on):
     if ser.is_open:
@@ -23,10 +40,19 @@ def switch_led(button_index , is_turn_on):
             cmd = ("/set Led" + str(button_index) + " on").encode()
         else:
             cmd = ("/set Led" + str(button_index) + " off").encode()
-    
+
         time.sleep(.1)
         ser.write(cmd)
         ser.flushOutput()
+
+def parse_config_file():
+    global config
+    # Opening JSON file
+    config_file = open("config.json", 'r' , encoding="utf-8")
+    config = json.load(config_file)
+    config_file.close()
+
+
 
 def parse_questions_file(file):
     result =[]
@@ -47,19 +73,15 @@ def parse_questions_file(file):
             result.append(entry)
     return result
 
-def clean_player_answers():
-    global played_buttons
-    played_buttons = []
-
-def print_scores(): 
+def print_scores():
     print("* Current Scores")
-    for i in range(nb_players):
-        print("Player "+ str(i) + " : " + str(player_scores[i]))
+    for i in range(config["nb_players"]):
+        print(player_names[i] + " : " + str(player_scores[i]))
     print("****")
 
 def print_question(question):
-    print("Question: " + question[0]) 
-    print("Reponse: " + question[1]) 
+    print("Question: " + question[0])
+    print("Reponse: " + question[1])
 
 class SerialThread(Thread):
     def __init__(self):
@@ -80,7 +102,6 @@ class SerialThread(Thread):
             time.sleep(0.1)
 
 
-
 class GameThread(Thread):
     def __init__(self):
         Thread.__init__(self)
@@ -88,63 +109,64 @@ class GameThread(Thread):
         self.start()
     def run(self):
 
-        global is_game_on
+        global is_game_on, played_buttons
+        for question in questions:
 
-        index = 0
-
-        while index < len(questions):
-            question = questions[index]
             print_scores()
             print_question(question)
 
-            nb_current_players = nb_players
+            nb_current_players = config["nb_players"]
             is_correct = False
-            clean_player_answers()
+            played_buttons = []
 
             already_answer = []
 
-
-            while is_correct == False and nb_current_players > 0: 
+            while not is_correct and nb_current_players > 0:
 
                 if len(played_buttons) > 0:
                     id_player = played_buttons.pop()
                     if id_player >= 0:
 
                         nb_current_players -= 1
-                        print("For player " + str(id_player)  + ", the answer is correct (y/n) ?")
+                        print(player_names[id_player]  + ", the answer is correct (y/n) ?")
                         reponse = str(input())
 
                         print("")
                         if reponse == "y":
                             is_correct = True
-                        else: 
+                        else:
                             already_answer.append(id_player)
                             print_question(question)
-        
-                        clean_player_answers()
+
                     else:
                         print("ERROR : Did not recognize the player : " + str(id_player))
 
+                    played_buttons = []
+
+                if is_correct:
+                    player_scores[id_player] += config["points_per_good_answer"]
+                else:
+                    player_scores[id_player] += config["points_per_wrong_answer"]
+
                 time.sleep(0.1)
-                if is_correct == True:
-                   player_scores[id_player] += QUESTION_SCORE
-            index += 1
         is_game_on = False
 
 
-##### Config 
-nb_players = 4 
-nb_question = 10
-player_scores = [0] * nb_players
-QUESTION_SCORE = 1
+parse_config_file()
+questions = parse_questions_file(config["question_file"])
+
+
+player_scores = [0] * config["nb_players"]
+player_names = { k : "Player " + str(k) for k in range(config["nb_players"])}
 
 played_buttons = []
 
-questions = parse_questions_file("questions.txt")
-
+ser = serial_init()
 SerialThread()
-GameThread()
+
+# Launch a game
 is_game_on = True
+GameThread()
 
 while is_game_on:
     time.sleep(0.1)
@@ -153,4 +175,3 @@ print("Score Final: ")
 print_scores()
 
 ser.close()
-
